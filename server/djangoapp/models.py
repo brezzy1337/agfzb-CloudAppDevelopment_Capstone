4298bf85ca033955f35c28150cc2346f5ccac850
brezzy1337 <devin.phat97@gmail.com>
2024-06-13 14:11:28 -0400

    Updated CarModel with required attributes and and range for model year

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -2,16 +2,16 @@ from django.db import models
 from django.utils.timezone import now
 
 class CarMake(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
+    name = models.CharField(null=False, max_length=100)
+    description = models.TextField(null=True, max_length=500)
 
     def __str__(self):
         return self.name
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
-    name = models.CharField(max_length=99)
-
+    car_make = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=99)
+    dealer_id = models.IntegerField(null=True)
  
     TYPE_CHOICES = [
         ('SEDAN', 'Sedan'),
@@ -20,8 +20,14 @@ class CarModel(models.Model):
         ('HATCHBACK', 'Hatchback'),
         ('CONVERTIBLE', 'Convertible'),
     ]
-    type = models.CharField(max_length=19, choices=TYPE_CHOICES, default="SEDAN")
-    year = models.IntegerField(default=2023) 
+    model_type = models.CharField(null=False, max_length=19, choices=TYPE_CHOICES, default="SEDAN")
+
+    YEAR_CHOICES = []
+    for r in range(1969, (datetime.datetime.now().year+1)):
+        YEAR_CHOICES.append((r, r))
+
+    year = models.IntegerField( 
+        ('year'), choices=YEAR_CHOICES, default=datetime.datetime.now().year) 
 
 def __str__(self):
-        return self.name  # Return the name as the string representation
+        return self.name + ", " + str(self.year) + ", " + self.model_type
